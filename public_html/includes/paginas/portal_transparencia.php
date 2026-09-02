8454ca84964d85f418936c0717b9ab88534fd44e
Rossini <rossini_rodrigues_alves@hotmail.com>
2026-09-02 16:28:06 -0300

    Atualizar portal da transparência 2025
    Adiciona novos documentos de transparência para 2025/2026 e atualiza a página do portal com links para convênio, plano de trabalho, estatuto, atas, demonstrativos financeiros, quadro de dirigentes e certificados CEBAS. Também inclui os arquivos PDF correspondentes na pasta de transparência.

--- public_html/includes/paginas/portal_transparencia.php
+++ public_html/includes/paginas/portal_transparencia.php
@@ -27,6 +27,7 @@ h3 {
 			<div class='col-md-12'>
 <h2>Convênio</h2>
 <h3>Estadual</h3>
+<a href="/includes/paginas/transparencia/convenio-estadual-342-2025.pdf" target="_blank" download><p>- Convênio 342 - 2025</p></a>
 <a href="/includes/paginas/transparencia/convenio-383-2020.pdf" target=_blank><p>- Convênio 383 - 2020.</p></a>
 <a href="/includes/paginas/transparencia/Convenio_estadual_102_2021.pdf" target=_blank><p>- Convênio 102 - 2021</p></a>
 <a href="/includes/paginas/transparencia/Convenio%20estadual%20350_2021.pdf" target=_blank><p>- Convênio 350 - 2020-2023 </p></a>
@@ -39,14 +40,22 @@ h3 {
 <a href="/includes/paginas/transparencia/emenda-parlamentar-349-creditada-em-2022.pdf" target=_blank><p>- Emenda Parlamentar 349 - Creditada em 2022</p></a>
 <a href="/includes/paginas/transparencia/Convenio%20000280_2021%20emenda%20parlamentar.pdf" target=_blank><p>-  Emenda Parlamentar 280 - Creditada em 2021</p></a>
 <!-- <a href="/includes/paginas/transparencia/Convenio%20000349_2021%20emenda%20parlamentar.pdf" target=_blank><p>- Emenda Parlamentar 349 - 2021 </p></a> -->
+<h2>Planos de Trabalho</h2>
+<a href="/includes/paginas/transparencia/plano-trabalho-convenio-342-2025.pdf" target="_blank" download><p>- Plano de Trabalho - Convênio Estadual 342/2025</p></a>
 <h2>Estatuto</h2>
+<a href="/includes/paginas/transparencia/estatuto-santa-casa-lorena-2026.pdf" target="_blank" download><p>- Estatuto Santa Casa de Lorena - 2026</p></a>
 <a href="/includes/paginas/transparencia/Estatuto%20SCL%2005_05_2021.pdf" target=_blank><p>- Estatuto Santa Casa de Lorena </p></a>
+<h2>Atas</h2>
+<a href="/includes/paginas/transparencia/ata-eleicao-posse-santa-casa-2025.pdf" target="_blank" download><p>- Ata de Eleição e Posse - 2025</p></a>
 <h2>Quadro de Dirigentes</h2>
+<a href="/includes/paginas/transparencia/quadro-dirigentes-2025-2028.pdf" target="_blank" download><p>Quadro de Dirigentes Santa Casa de Lorena 2025-2028</p></a>
 <a href="/includes/paginas/transparencia/quadro-de-dirigentes-santa-casa-de-Lorena-2023-2024.pdf" target=_blank><p>Quadro de Dirigentes Santa Casa de Lorena 2023-2024</p></a>
 <a href="/includes/paginas/transparencia/Quadro_de_Dirigentes_e_Conselheiros2022-2023.pdf" target=_blank><p>Quadro de Dirigentes Santa Casa de Lorena 2022-2023</p></a>
 <a href="/includes/paginas/transparencia/Quadro_de_Dirigentes_e_Conselheiros2021-2022.pdf" target=_blank><p>Quadro de Dirigentes Santa Casa de Lorena 2021-2022</p></a>
 <a href="/includes/paginas/transparencia/Quadro_de_Dirigentes_e_Conselheiros2018-2021.pdf" target=_blank><p>Quadro de Dirigentes Santa Casa de Lorena 2018-2021</p></a>
 <h2>Demonstrações Financeiras</h2>
+<a href="/includes/paginas/transparencia/demonstracoes-financeiras-2024-2023.pdf" target="_blank" download><p>Demonstrações Financeiras - 2024 e 2023</p></a>
+<a href="/includes/paginas/transparencia/demonstracoes-financeiras-2023-2022.pdf" target="_blank" download><p>Demonstrações Financeiras - 2023 e 2022</p></a>
 <a href="/includes/paginas/transparencia/demonstracoes-financeiras-2022.pdf" target=_blank><p>Demonstrações Financeiras - 2022</p></a>
 <a href="/includes/paginas/transparencia/Demonstracoes_Financeiras_2021.pdf" target=_blank><p>Demonstrações Financeiras - 2021</p></a>
 <a href="/includes/paginas/transparencia/Demonstracoes_Financeiras_2020.pdf" target=_blank><p>Demonstrações Financeiras - 2020</p></a>
@@ -54,6 +63,9 @@ h3 {
 <a href="/includes/paginas/transparencia/Demonstracoes_Financeiras_2018.pdf" target=_blank><p>Demonstrações Financeiras - 2018</p></a>
 <a href="/includes/paginas/transparencia/Demonstracoes_Financeiras_2017.pdf" target=_blank><p>Demonstrações Financeiras - 2017</p></a>
 <a href="/includes/paginas/transparencia/Demonstracoes_Financeiras_2016.pdf" target=_blank><p>Demonstrações Financeiras - 2016</p></a>
+<h2>CEBAS</h2>
+<a href="/includes/paginas/transparencia/cebas-validade-31-12-2025.pdf" target="_blank" download><p>Certificado CEBAS - Validade até 31/12/2025</p></a>
+<a href="/includes/paginas/transparencia/declaracao-tempestividade-cebas-2025-2026.pdf" target="_blank" download><p>Declaração de Tempestividade CEBAS - 2025/2026</p></a>
 <h2>Programa de Especialização Em Medicina Intensiva (PEMI/AMIB)</h2>
 <a href="/includes/paginas/transparencia/PEMI_2026-2029.pdf" target=_blank><p>Programa de Especialização Em Medicina Intensiva - 2026 - 2029</p></a>
 			</div>
